91dd98443cf2910f6ad3820a22694bbcd074176c
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-21 20:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
-
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".btn");
     const stopButton = document.querySelector(".stop");
     const audio = document.getElementById("audio");
 
-    for(let i = 0; i<buttons.length; i++){
-        buttons.addEventListener("click", function(){
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
             const soundFile = this.getAttribute("data-sound");
-            audio.src =   `sounds\${soundFile}`;
-            audio.play();  
-        })
-    }
+            audio.src = `sounds/${soundFile}`;
+            audio.play();
+        });
+    });
 
     stopButton.addEventListener("click", function () {
         audio.pause();
         audio.currentTime = 0;
     });
-
-})
+});
